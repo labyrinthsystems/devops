5acfb8978beca2de86c9dc8f1c325892ad08ff88
Gerard <a@b.c>
2017-04-27 16:37:54 +0100

    Heroku doesn't like invoke method...

--- src/main/java/maven1/Calculator.java
+++ src/main/java/maven1/Calculator.java
@@ -4,8 +4,11 @@ import javax.swing.*;
 public class Calculator {
 	public static void main(String[] args) {
 
-		SwingUtilities.invokeLater(CalculatorView::new);
+		//SwingUtilities.invokeLater(CalculatorView::new);
 		//SwingUtilities.invokeNOW!!!(CalculatorView::new);
+		
+		CalculatorView cv = new CalculatorView();
+		cv.setVisible(true);
 
 	}
 }
